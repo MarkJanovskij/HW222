a952a511de77826ac7691496d018cc3a6ea094a7
Mark <markyanovskiy@gmail.com>
2023-10-25 01:04:42 +0200

    Calculator Mid Score Mark Janovskij

---
+++ task_score.js
@@ -0,0 +1,32 @@
+введение
+// Name Mark Janovskij Score 10, 20, 30, 40, 50, 60, 70, 80 ,90 ,100 Mid score
+- git
+
+- js
+let name = "Mark Janovskij"
+let score1 = 10
+let score2 = 20
+let score3 = 30
+let score4 = 40
+let score5 = 50
+let score6 = 60
+let score7 = 70
+let score8 = 80
+let score9 = 90
+let score10 = 100
+
+let scoreTotal = score1 + score2 + score3 + score4 + score5 + score6 + score7 + score8 +score9 + score10 
+
+let scoreMid = scoreTotal / 10
+
+let stringType1 = "Total Score";
+console.log(stringType1)
+console.log(scoreTotal);
+
+let stringType2 = "Full Name";
+console.log(stringType2)
+console.log(name);
+
+let stringType3 = "Middle Score";
+console.log(stringType3)
+console.log(scoreMid);
